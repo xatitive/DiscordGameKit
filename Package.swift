116b62f34790e443d7bb9017b3f7bfb32dd6c487
Christian <83311493+xatitive@users.noreply.github.com>
2026-04-05 17:30:47 -0400

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version: 6.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "DiscordGameKit",
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .executableTarget(
+            name: "DiscordGameKit"
+        ),
+
+    ],
+    swiftLanguageModes: [.v6]
+)
---
+++ Sources/DiscordGameKit/DiscordGameKit.swift
@@ -0,0 +1,9 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+@main
+struct DiscordGameKit {
+    static func main() {
+        print("Hello, world!")
+    }
+}

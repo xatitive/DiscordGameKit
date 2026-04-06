ae9b918c996b1d7794dcb3513eb3953f367933de
Christian <83311493+xatitive@users.noreply.github.com>
2026-04-05 22:52:58 -0400

    Changed the authorize parameter 'args' to 'with'. Changed the documentation comments as well.

--- Sources/DiscordGameKit/Wrappers/Client/DiscordClient.swift
+++ Sources/DiscordGameKit/Wrappers/Client/DiscordClient.swift
@@ -186,8 +186,8 @@ public final class DiscordClient: DiscordObject, @unchecked Sendable {
 
     /// Authorization helper property that can create a code challenge and verifier.
     ///
-    /// Used in the ``authorize(args:_:)`` + ``getToken(application:code:codeVerifier:redirectUri:_:)`` flow.
-    /// This returns a struct with two items, a ``AuthorizationCodeVerifier/challenge`` value to pass into ``authorize(args:_:)`` and
+    /// Used in the ``authorize(with:_:)`` + ``getToken(application:code:codeVerifier:redirectUri:_:)`` flow.
+    /// This returns a struct with two items, a ``AuthorizationCodeVerifier/challenge`` value to pass into ``authorize(with:_:)`` and
     ///  a ``AuthorizationCodeVerifier/verifier`` value to pass into ``getToken(application:code:codeVerifier:redirectUri:_:)``.
     public lazy var authorizationCodeVerifier: AuthorizationCodeVerifier = {
         storage.withLock { raw in
